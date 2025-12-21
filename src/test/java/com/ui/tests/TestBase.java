e711d90213b9c601a872ab0ce64ee0a710c81aa9
Anand <preetpank@gmail.com>
2025-12-22 01:35:15 +0530

    added parameters

--- src/test/java/com/ui/tests/TestBase.java
+++ src/test/java/com/ui/tests/TestBase.java
@@ -1,12 +1,12 @@
 package com.ui.tests;
 
-import static com.constants.Browser.EDGE;
-
 import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
 
 import com.constants.Browser;
 import com.ui.pages.HomePage;
@@ -19,18 +19,23 @@ public class TestBase {
 	protected HomePage homePage;
 	Logger logger = LoggerUtility.getLogger(this.getClass());
 
-	private boolean isLamdaTest = true;
-	private boolean isHeadless = true;
+	private boolean isLamdaTest;
 
+	@Parameters({ "browser", "isLamdaTest", "isHeadless" })
 	@BeforeMethod(description = "Load the homepage of the website")
-	public void setUp(ITestResult result) {
+	public void setUp(@Optional("Chrome")String browser, 
+			@Optional("false") boolean isLambdaTest, 
+			@Optional("false") boolean isHeadless, ITestResult result) {
+
+		this.isLamdaTest = isLamdaTest;
+
 		WebDriver lambdaDriver;
 		if (isLamdaTest) {
-			lambdaDriver = LambdaTestUtility.intializeLambdaTestSession("chrome", result.getMethod().getMethodName());
+			lambdaDriver = LambdaTestUtility.intializeLambdaTestSession(browser, result.getMethod().getMethodName());
 			homePage = new HomePage(lambdaDriver);
 		} else {
 			logger.info("HomePage of the website");
-			homePage = new HomePage(Browser.CHROME, isHeadless);
+			homePage = new HomePage(Browser.valueOf(browser.toUpperCase()), isHeadless);
 		}
 	}
 
